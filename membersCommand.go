bdd62fc3f1ae5245441b97eb43045d5e5b874a2c
Hunter <kepley.l.hunter@gmail.com>
2018-03-12 21:47:30 -0400

    Changed members command to be more precise
    Shows bots/members separately.

--- membersCommand.go
+++ membersCommand.go
@@ -13,9 +13,18 @@ func membersCommand(s *discordgo.Session, m *discordgo.MessageCreate) { // Displ
 		fmt.Println("Unable to get guild")
 		log.Fatal(err)
 	} else {
+		amount := 0
+		bots := 0
+		for i := 0; i < len(currentGuild.Members); i++ {
+			if !currentGuild.Members[i].User.Bot {
+				amount++
+			} else {
+				bots++
+			}
+		}
 		s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
 			Title:       fmt.Sprintf("%s", currentGuild.Name),
 			Thumbnail:   &discordgo.MessageEmbedThumbnail{discordgo.EndpointGuildIcon(currentGuild.ID, currentGuild.Icon), "", 30, 30},
-			Description: fmt.Sprintf("There are %d members!", currentGuild.MemberCount)})
+			Description: fmt.Sprintf("There are `%d` member[s] and `%d` bot[s]", amount, bots)})
 	}
 }
